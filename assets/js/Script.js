1cea045ca538d8355542605cf13870ef28916a7a
Juan Granda <juanfromecuador@gmail.com>
2016-08-02 18:34:07 -0400

    layout edits / console-pics js /smooth scroll-aboutme

--- assets/js/Script.js
+++ assets/js/Script.js
@@ -6,23 +6,9 @@ $(function () {
     }, 500);
 });
 
-function showRPS() {
-    $('#rps').fadeOut(50, function () {
-        $('#rps').attr("src", 'images/RPS-menu-scrn-pic.jpg');
-    }).slideDown(900);
-}
-function hideRPS() {
-    $('#rps').slideUp(600, function () {
-        $('#rps').attr("src", '');
-    }).fadeOut(100);
-}
-function showRPS2() {
-    $('#rps2').fadeOut(50, function () {
-        $('#rps2').attr("src", 'images/RPS-vs-scrn-pic.jpg');
-    }).slideDown(900);
-}
-function hideRPS2() {
-    $('#rps2').slideUp(600, function () {
-        $('#rps2').attr("src", '');
-    }).fadeOut(100);
-}
+$(document).ready(function(){
+    $('#hideTest').hide();
+        $('#rps-scrn-shots').click(function () {
+                $('#hideTest').toggle(300);
+    });
+})
