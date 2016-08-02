484a291fef573d8536ec04a3ee577fb4325dc72e
Juan Granda <juanfromecuador@gmail.com>
2016-08-02 08:54:25 -0400

    added 1st concole app

--- assets/js/Script.js
+++ assets/js/Script.js
@@ -5,3 +5,24 @@ $(function () {
         $('.animationList').removeClass('hidden');
     }, 500);
 })();
+
+function showRPS() {
+    $('#rps').fadeOut(50, function () {
+        $('#rps').attr("src", 'images/RPS-menu-scrn-pic.jpg');
+    }).slideDown(900);
+}
+function hideRPS() {
+    $('#rps').slideUp(600, function () {
+        $('#rps').attr("src", '');
+    }).fadeOut(100);
+}
+function showRPS2() {
+    $('#rps2').fadeOut(50, function () {
+        $('#rps2').attr("src", 'images/RPS-vs-scrn-pic.jpg');
+    }).slideDown(900);
+}
+function hideRPS2() {
+    $('#rps2').slideUp(600, function () {
+        $('#rps2').attr("src", '');
+    }).fadeOut(100);
+}
